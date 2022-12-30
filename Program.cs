aaae4981fbb561f811b5a326028ae94cca32afd2
Hananayman44 <121646501+Hananayman44@users.noreply.github.com>
2022-12-30 17:00:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 12; number++)
+            {
+                int sum = 0;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        sum++;
+                    }
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(number);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
